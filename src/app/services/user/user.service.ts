5fc221b2ed7c556870785a1099bff7111dd73316
Miguel-Florian <migflor04@gmail.com>
2022-06-25 20:45:27 +0100

    Creating user services

---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,56 @@
+/* eslint-disable @typescript-eslint/semi */
+/* eslint-disable object-shorthand */
+/* eslint-disable @typescript-eslint/quotes */
+/* eslint-disable @typescript-eslint/naming-convention */
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { User } from 'src/models/users.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  users: User[];
+
+  constructor(private alertController: AlertController, private router: Router) {
+    this.users = [
+      {
+        Firstname: 'Miguel',
+        Lastname: 'Florian',
+        Email: 'migflor@gmail.com',
+        Password: 'mimiflo',
+      }
+    ];
+  }
+
+  Login(email: string, code: string) {
+    if (email === 'migflor@gmail.com' && code === 'mimiflo') {
+      this.router.navigateByUrl('/home');
+    } else {
+      this.onShowAlert('Credentials incorrects');
+    }
+  }
+  Register(nom: string, prenom: string, mail: string, code: string,) {
+    this.users.push({
+      Firstname: nom,
+      Lastname: prenom,
+      Email: mail,
+      Password: code,
+    });
+    console.log(this.users);
+  }
+  GetAllUsers() {
+    return this.users;
+  }
+
+  async onShowAlert(message: string) {
+    const alert = await this.alertController.create({
+      header: "Information",
+      message: message,
+      cssClass: "customAlert",
+      buttons: ['OK']
+    });
+    alert.present()
+  }
+}
